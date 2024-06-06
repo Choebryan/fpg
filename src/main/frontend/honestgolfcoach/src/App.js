a470b49e9d6075a1cca826393aab60087ed6de92
Bryan Choe <choe@bryans-mbp-3.lan>
2024-06-06 03:07:33 -0700

    added lesson form with submission functionality, need to add ability to email next

--- src/main/frontend/honestgolfcoach/src/App.js
+++ src/main/frontend/honestgolfcoach/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import ResponsiveAppBar from './home/components/appBar';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Home from './home/home';
-import Lessons from './home/lessons';
+import BookLesson from './home/bookLesson';
 
 function App() {
   return (
     <Routes>
       {/* <div className='App'> */}
       <Route path='/' element={<Home />} />
-      <Route path="lessons" element={<Lessons />}/>
+      <Route path="lessons" element={<BookLesson />}/>
       {/* </div> */}
     </Routes>
   );
