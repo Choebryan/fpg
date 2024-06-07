def967385d3978cad114a065cdde3949546066b2
Bryan Choe <choebryan3@gmail.com>
2024-06-07 02:59:29 -0700

    pushed java changes

---
+++ src/main/java/com/foolproofgolf/fpg/model/VideoRecording.java
@@ -0,0 +1,27 @@
+package com.foolproofgolf.fpg.model;
+
+import java.time.LocalDateTime;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Document(collection = "videoRecordings")
+@Data
+@AllArgsConstructor
+public class VideoRecording {
+    
+    @Id
+    private ObjectId id;
+
+    private ObjectId lessonId;
+
+    private String videoTitle;
+
+    private String videoUrl;
+
+    private LocalDateTime recordingDateTime;
+}
--- src/main/java/com/foolproofgolf/fpg/service/UserService.java
+++ src/main/java/com/foolproofgolf/fpg/service/UserService.java
@@ -1,9 +1,9 @@
-package com.foolproofgolf.fpg.service;
+// package com.foolproofgolf.fpg.service;
 
-import com.foolproofgolf.fpg.models.User;
+// import com.foolproofgolf.fpg.model.User;
 
-public interface UserService {
-    public User saveUser(User user);
+// public interface UserService {
+//     public User saveUser(User user);
 
-    public User getUserByUsername(String username);
-}
+//     public User getUserByUsername(String username);
+// }
